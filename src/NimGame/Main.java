51b3dc88e231a47a17bf5410f313b55f9e593b20
xhaiben <xhaiben@hotmail.com>
2017-02-06 10:29:18 +0800

    Nim Game

---
+++ src/NimGame/Main.java
@@ -0,0 +1,14 @@
+package NimGame;
+
+/**
+ * Created by xhaiben on 2017/2/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canWinNim(int n) {
+        return n % 4 == 0 ? false : true;
+    }
+}

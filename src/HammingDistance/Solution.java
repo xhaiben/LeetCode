fbaa2b184af37de7be5898e3a46da08f0909e615
xhaiben <xhaiben@hotmail.com>
2017-02-10 22:05:40 +0800

    commit

--- src/HammingDistance/Solution.java
+++ src/HammingDistance/Solution.java
@@ -4,6 +4,17 @@ package HammingDistance;
  * Created by xhaiben on 2017/2/4.
  */
 public class Solution {
+    /**
+     * hamming Distance 两个字符串不同字符的个数
+     * 100110
+     * 110111
+     * 不同的字符有2个
+     * 所以其hamming Distance为2
+     * 采用异或 将不同的位置为1 统计1的个数
+     * @param x
+     * @param y
+     * @return
+     */
     public int hammingDistance(int x, int y) {
         int z = x ^ y;
         int count = 0;
---
+++ src/NextGreaterElementI/Main.java
@@ -0,0 +1,11 @@
+package NextGreaterElementI;
+
+/**
+ * Created by xhaiben on 2017/2/8.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
